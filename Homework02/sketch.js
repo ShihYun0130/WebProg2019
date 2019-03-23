6da405ac632189c5f303f6872d723dbb99556ab8
Shih Yun Chen <cinnacinna130@gmail.com>
2019-03-24 04:24:02 +0800

    react.js 1

--- Homework02/sketch.js
+++ Homework02/sketch.js
@@ -14,6 +14,8 @@ let start_flag;
 let xr, yr;
 let base_x;
 let loop_flag;
+let pipe_x, pipe_y;
+let pipes;
 // assets from: https://github.com/sourabhv/FlapPyBird/tree/master/assets
 
 function preload() {
@@ -26,6 +28,11 @@ function preload() {
             flap => loadImage(`assets/sprites/${color}bird-${flap}.png`)
         )
     );
+    pipes = ["green", "red"].map(
+        color => ["upper", "lower"].map(
+            position => loadImage(`assets/sprites/pipe-${color}-${position}.png`)
+        )
+    ); 
     sound_wing = loadSound("assets/audio/wing.wav");
     sound_hit = loadSound("assets/audio/hit.wav");
     sound_die = loadSound("assets/audio/die.wav");
@@ -48,95 +55,102 @@ function setup() {
     readyScale = width/readyImg.width;
     baseScale = width/baseImg.width;
     triAng = 0;
-    vx = 1;
+    vx = 2;
 	vy = 0
 	ay = 10;
 	xb = width/2 - assets[0][0].width/2;
-    yb = height/2 - assets[0][0].height/2;
+    yb = -(height-baseImg.height*baseScale)/2 + assets[0][0].width;
     flap_num = 0;
     color_index = Math.floor(Math.random() * 3);
     mode_index = Math.floor(Math.random() * 2);
     start_flag = 0;
-    xr = width/2 - readyImg.width/2;
+    xr = -readyImg.width/2;
     yr = height/2 - readyImg.height/2;
-    base_x = 0;
+    base_x = -width;
     loop_flag = 0;
+    pipe_x = width;
 }
 
 function draw() {
     // Render function (called per frame.)
     background(0);
     x1 -= 1;
-    // console.log("1", x1);
+    base_x -= 1;
+    //console.log("1", pipe_x);
     if (x1 <= -bgImg[mode_index].width*bgScale) 
     {
+        //console.log("base_x", base_x);
         x1 += bgImg[mode_index].width*bgScale;
         xb -= bgImg[mode_index].width*bgScale;
-        xr -= bgImg[mode_index].width*bgScale;
-        //base_x += bgImg[mode_index].width*bgScale;
-        // console.log(x1 + bgImg[mode_index].width*bgScale);
+        xr -= width;
+        pipe_x += width; 
+        base_x = -width;
+        //console.log("x1", x1);
     }
     translate(x1, y1);
-    image(bgImg[mode_index], 0, 0, bgImg[mode_index].width*bgScale, bgImg[mode_index].height*bgScale);
-    image(bgImg[mode_index], bgImg[mode_index].width*bgScale, 0, bgImg[mode_index].width*bgScale, bgImg[mode_index].height*bgScale);
+    image(bgImg[0], 0, 0, bgImg[mode_index].width*bgScale, bgImg[mode_index].height*bgScale);
+    image(bgImg[1], bgImg[mode_index].width*bgScale, 0, bgImg[mode_index].width*bgScale, bgImg[mode_index].height*bgScale);
     
-    // base_x -= 1;
-    // // console.log("3", base_x, width);
-    // //console.log(base_x, width);
+    // random height and color of pipe
+    pipe();
+
     
-    // //console.log("4", base_x, width);
+    xr += 3;
+    console.log("base", base_x);
+    if (base_x <= -width*14/6) 
+    {
+        console.log("width", width);
+        base_x += width*14/6;
+        xr -= width;
+        xb -= bgImg[mode_index].width*bgScale;
+        //pipe_x -= pipes[0][0].width;
+        //pipe_x += width;
+        //console.log("base", base_x, xr, pipe_x);
+    }
     
-    // if (base_x <= -baseImg.width*baseScale) 
-    // {
-    //     base_x = base_x+  baseImg.width*baseScale;
-    //     console.log("4", base_x, width);
-    // }
-    // translate(base_x, height-baseImg.height*baseScale);
-    // image(baseImg, 0, 0, baseImg.width*baseScale, baseImg.height*baseScale);
-    // //console.log("scale",baseImg.width*baseScale);
-    // image(baseImg, baseImg.width*baseScale, 0, baseImg.width*baseScale, baseImg.height*baseScale);
+    translate(base_x, height-baseImg.height*baseScale);
+    image(baseImg, 0, 0, baseImg.width*baseScale, baseImg.height*baseScale);
+    image(baseImg, width, 0, baseImg.width*baseScale, baseImg.height*baseScale);
+    
+    
+
     // random color of bird
     flap_num += 1;
     flap_index = flap_num % 3
 
     xb += vx;
-    if (loop_flag === 0){
-        if (start_flag === 0) {
-            getReady();
-        }
-        else {
-            // flappy bird fly
-            vy += ay*0.04;
-            yb += vy;
-            triAng += 0.03;
-            translate(xb, yb);
-            rotate(triAng);
-            image(assets[color_index][flap_index], 0, 0, assets[color_index][0].width, assets[color_index][0].height);
-    
-            // hit and die
-            console.log(yb+assets[color_index][0].height/2);
-            console.log("height", height);
-            if (yb+5 >= height || yb === 0) {
-                //console.log(yb);
-                vx = 0;
-                vy = 0;
-                ay = 0;
-                sound_hit.play();
-                sound_die.play();
-                loop_flag = 1;
-                translate(xb, yb);
-                noLoop();
-                // push();
-                // image(assets[color_index][flap_index], 0, 0, assets[color_index][0].width, assets[color_index][0].height);
-            }
-            //loop();
-        }
+    if (start_flag === 0) {
+        getReady();
     }
-    else{
-        loop();
-        loop_flag === 0;
+    else {
+        // flappy bird fly
+        vy += ay*0.04;
+        yb += vy;
+        triAng += 0.03;
+        translate(xb, yb);
+        rotate(triAng);
         image(assets[color_index][flap_index], 0, 0, assets[color_index][0].width, assets[color_index][0].height);
+
+        // hit and die
+        if (yb+5 >= height || yb === 0) {
+            //console.log(yb);
+            vx = 0;
+            vy = 0;
+            ay = 0;
+            sound_hit.play();
+            sound_die.play();
+            loop_flag = 1;
+            translate(xb, yb);
+            console.log("die");
+            noLoop();
+        }
+
+        
+        
+        
+        
     }
+   
     
     
 }
@@ -152,8 +166,7 @@ function keyPressed() {
 
 function getReady() {
     // message
-    xr += 1;
-    translate(xr, yr);
+    translate(xr, -height/2);
     image(readyImg, 0, 0, readyImg.width, readyImg.height);
     // flappy bird
     translate(readyImg.width/2-assets[color_index][0].width/2, readyImg.height/2+assets[color_index][0].height);
@@ -163,4 +176,18 @@ function getReady() {
 // for getReady
 function mousePressed() {
     start_flag = 1;
+}
+
+function pipe() {
+    pipe_x -= 1;
+    // if (pipe_x <= 0) 
+    // {
+    //     pipe_x += width;
+    //     console.log("pipe_x", pipe_x, width);
+    // }
+
+    //console.log("pipe_x", pipe_x, width);
+    translate(pipe_x, 0);
+    image(pipes[0][0], 0, 0, pipes[0][0].width, pipes[0][0].height);
+    image(pipes[0][0], width/2, 0, pipes[0][0].width, pipes[0][0].height);
 }
